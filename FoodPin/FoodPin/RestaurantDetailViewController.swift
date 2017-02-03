16ac3b18af573414d2c8941252753de524965889
Ben <m4ruanuwaterloo@gmail.com>
2017-02-03 16:11:02 -0500

    Added detail page

---
+++ FoodPin/FoodPin/RestaurantDetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  RestaurantDetailViewController.swift
+//  FoodPin
+//
+//  Created by Ben on 2017-02-03.
+//  Copyright © 2017 Ben. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailViewController: UIViewController {
+
+    @IBOutlet var restaurantImageView:UIImageView!
+    var restaurantImage = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        restaurantImageView.image = UIImage(named: restaurantImage)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- FoodPin/FoodPin/RestaurantTableViewController.swift
+++ FoodPin/FoodPin/RestaurantTableViewController.swift
@@ -75,48 +75,48 @@ class RestaurantTableViewController: UITableViewController {
         
         return cell
     }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let optionMenu = UIAlertController(title: nil, message: "What do you want to do", preferredStyle: .actionSheet)
-        
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler:
-            nil)
-        
-        let callActionHandler = { (action:UIAlertAction!) -> Void in
-            let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry, the call feature is not available yet. Please retry later.", preferredStyle: .alert)
-            
-            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                
-            self.present(alertMessage, animated: true, completion: nil)
-        }
-        
-        let callAction = UIAlertAction(title: "Call " + "123-000-\(indexPath.row)",
-            style: .default, handler: callActionHandler)
-        optionMenu.addAction(callAction)
-        
-        let checkInAction : UIAlertAction
-        if !self.restaurantIsVisited[indexPath.row] {
-            checkInAction = UIAlertAction(title: "Check in", style: .default, handler: { (action:UIAlertAction!) -> Void in
-                let cell = tableView.cellForRow(at: indexPath)
-                cell?.accessoryType = .checkmark
-                self.restaurantIsVisited[indexPath.row] = true
-            })
-        } else {
-            checkInAction = UIAlertAction(title: "Undo Check in", style: .default, handler: { (action:UIAlertAction!) -> Void in
-                let cell = tableView.cellForRow(at: indexPath)
-                cell?.accessoryType = .none
-                self.restaurantIsVisited[indexPath.row] = false
-            })
-        }
-        
-        optionMenu.addAction(checkInAction)
-        
-        optionMenu.addAction(cancelAction)
-        // Display the menu
-        present(optionMenu, animated: true, completion: nil)
-        
-        tableView.deselectRow(at: indexPath, animated: false)
-    }
+//    
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        let optionMenu = UIAlertController(title: nil, message: "What do you want to do", preferredStyle: .actionSheet)
+//        
+//        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler:
+//            nil)
+//        
+//        let callActionHandler = { (action:UIAlertAction!) -> Void in
+//            let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry, the call feature is not available yet. Please retry later.", preferredStyle: .alert)
+//            
+//            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+//                
+//            self.present(alertMessage, animated: true, completion: nil)
+//        }
+//        
+//        let callAction = UIAlertAction(title: "Call " + "123-000-\(indexPath.row)",
+//            style: .default, handler: callActionHandler)
+//        optionMenu.addAction(callAction)
+//        
+//        let checkInAction : UIAlertAction
+//        if !self.restaurantIsVisited[indexPath.row] {
+//            checkInAction = UIAlertAction(title: "Check in", style: .default, handler: { (action:UIAlertAction!) -> Void in
+//                let cell = tableView.cellForRow(at: indexPath)
+//                cell?.accessoryType = .checkmark
+//                self.restaurantIsVisited[indexPath.row] = true
+//            })
+//        } else {
+//            checkInAction = UIAlertAction(title: "Undo Check in", style: .default, handler: { (action:UIAlertAction!) -> Void in
+//                let cell = tableView.cellForRow(at: indexPath)
+//                cell?.accessoryType = .none
+//                self.restaurantIsVisited[indexPath.row] = false
+//            })
+//        }
+//        
+//        optionMenu.addAction(checkInAction)
+//        
+//        optionMenu.addAction(cancelAction)
+//        // Display the menu
+//        present(optionMenu, animated: true, completion: nil)
+//        
+//        tableView.deselectRow(at: indexPath, animated: false)
+//    }
     
 //    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
 //        
@@ -160,49 +160,13 @@ class RestaurantTableViewController: UITableViewController {
         return [deleteAction, shareAction]
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "showRestaurantDetail" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let destinationController = segue.destination as! RestaurantDetailViewController
+                destinationController.restaurantImage = restaurantImages[indexPath.row]
+            }
+        }
     }
-    */
-
+    
 }
